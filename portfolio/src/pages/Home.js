9ef389c0f3e42cf7fb905e301659038fb624bbbe
Sonal <sonal1024jk@gmail.com>
2023-10-16 22:20:11 +0530

    commit and push

--- portfolio/src/pages/Home.js
+++ portfolio/src/pages/Home.js
@@ -15,13 +15,14 @@ const Home = () => {
       <p id="p2">I enjoy creating websites, programs and designs.</p>
       <p id="p3">With your vision and my ideas, we can create something beautiful.</p>
     </div>
-    <div className='socials'><div>
+    <div className='socials'><div id='links'>
       <Link to='https://github.com/sonal-jk' target='_blank'><img src={git} alt="github"></img></Link>
       <Link to='https://www.linkedin.com/in/sonal-jakhmola-15461022a' target='_blank'><img src={lin} alt="linkedin"></img></Link>
       <Link to='https://mail.google.com/a/saitm.org/?view=cm&fs=1&to=sonal1024jk@gmail.com' target='_blank'><img src={mail} alt="gmail"></img></Link>
       </div>
+      <div id='resume'>
       <Link to={resume} target='_blank'><button>My Resume</button></Link>
-      </div>
+      </div></div>
       </div>
     </div>
     </div>;
