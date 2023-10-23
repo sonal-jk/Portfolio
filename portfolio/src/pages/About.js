e000688100b4a8146ba46c112b3963e27396f02f
Sonal <sonal1024jk@gmail.com>
2023-10-23 21:04:06 +0530

    commit and push

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -1,5 +1,10 @@
 import ghost from '../images/Rectangle 3.png'
 import '../style.scss'
+import { Link } from 'react-router-dom';
+import git from '../images/git.png'
+import sql from '../images/sql.png'
+import program from '../images/Program.png'
+import web from '../images/web.png'
 const About = () => {
 
     return(  <div> <div className='about'>
@@ -16,9 +21,21 @@ const About = () => {
       <div className='skillset'>
 <h1>SKILL SET</h1>  
 <div>
-   <div>
-    <div>1</div>
-    <div>2</div></div> 
+   <div className='grid1'>
+    <div>
+      <img src={web} alt='web'></img>
+      <strong>WEB DEVELOPMENT</strong>
+    </div>
+    <div>
+      <img src={program} alt='program'></img>
+      <strong>PROGRAMMING</strong></div></div>
+    <div className='grid2'>
+    <div>
+      <img src={sql} alt='sql'></img>
+      <strong>DATABASE</strong></div>
+    <div>
+      <img src={git} alt='git'></img>
+      <strong>VERSION CONTROL</strong></div></div> 
 </div>
       </div>
       </div>);
