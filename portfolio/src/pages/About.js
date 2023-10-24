bad438d29e4800cf9e51234a7d86e8ae673caca2
Sonal <sonal1024jk@gmail.com>
2023-10-24 21:49:06 +0530

    commit and push

--- portfolio/src/pages/About.js
+++ portfolio/src/pages/About.js
@@ -5,6 +5,7 @@ import git from '../images/git.png'
 import sql from '../images/sql.png'
 import program from '../images/Program.png'
 import web from '../images/web.png'
+import robo from '../images/robo-3d.png'
 const About = () => {
 
     return(  <div> <div className='about'>
@@ -38,6 +39,13 @@ const About = () => {
       <strong>VERSION CONTROL</strong></div></div> 
 </div>
       </div>
+
+<div className='projects'>
+  <strong id='headline'>RECENT PROJECTS</strong>
+  <div id='pro1'><div className='panel'><img src={robo} alt='robo'></img></div>
+  <div className='panel'><p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+  <strong>Robo 3D</strong></div></div>
+</div>
       </div>);
   };
   
